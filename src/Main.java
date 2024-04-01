d516b3ea739be7f59683f91e61d34fed34d4694f
Jeffrey Roossien <jr.roossien@gmail.com>
2024-04-01 17:01:17 +0200

    Adapter pattern demo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 import Command.PauseCommand;
 import Command.PlayCommand;
 import Command.Player;
+import Media.Categorie;
+import Media.Film;
+import Media.StommeOndertiteling;
+import Media.StommeOndertitelingAdapter;
+
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +18,17 @@ public class Main {
         // Buttons worden normaal gesproken uitgevoerd door de GUI
         player.play();
         player.pause();
+
+        // Adapter pattern demo
+        HashMap<String, Integer> ondertitelRegels = new HashMap<>();
+        ondertitelRegels.put("Deze regel tekst komt voor in de eerste seconde.", 1);
+        ondertitelRegels.put("Ik heb niks gedaan! Wat gebeurt er???", 24);
+        ondertitelRegels.put("Inspirationele tekst hier", 15);
+        StommeOndertiteling stommeOndertiteling = new StommeOndertiteling("Nederlands", "srt", ondertitelRegels);
+        // Werkt niet!! Stomme ondertiteling werkt niet met Film.
+//        Film film = new Film("The Shawshank Redemption", null, stommeOndertiteling, 142, "Two imprisoned", Categorie.ACTIE);
+        StommeOndertitelingAdapter adapter = new StommeOndertitelingAdapter(stommeOndertiteling);
+        // Werkt wel! Adapter zorgt ervoor dat stomme ondertiteling werkt met Film.
+        Film film = new Film("The Shawshank Redemption", null, adapter.getOndertiteling(), 142, "Two imprisoned", Categorie.ACTIE);
     }
-    private Player player;
 }
